2b27dafd2f9c43b121a6ea7a6286981f0b201622
carloscafejava <carloscafe.java@gmail.com>
2025-10-16 00:30:07 +0000

    Correção atividade livro

---
+++ atividades/atividades/src/App.java
@@ -0,0 +1,10 @@
+import livroJava.Livros;
+
+public class App {
+    public static void main(String[] args) {
+        Livros livro = new Livros("Java para Leigos", "Barry A. Burd", 2006, 424);
+         livro.exibirInformacoes();
+
+
+    }
+}
---
+++ atividades/atividades/src/livroJava/Livros.java
@@ -0,0 +1,69 @@
+package livroJava; 
+
+public class Livros {
+  private String titulo; 
+  private String autor; 
+  private int anoPublicacao; 
+  private int quantidadePaginas; 
+
+  public Livros(String titulo, String autor, int anoPublicacao, int quantidadePaginas  ){
+        this.titulo = titulo; 
+        this.autor = autor; 
+        this.anoPublicacao = anoPublicacao; 
+        this.quantidadePaginas = quantidadePaginas; 
+  }
+
+  public String getTitulo(){
+    return titulo; 
+  }
+
+  public void setTitulo(String titulo){
+    this.titulo = titulo; 
+  }
+
+  public String getAutor(){
+    return autor; 
+  }
+
+  public void setAutor(String autor){
+    this.autor = autor; 
+  }
+
+public int getAnoPublicacao() {
+    return anoPublicacao; 
+}
+
+public void setAnoPublicacao(int anoPublicacao){
+    this.anoPublicacao = anoPublicacao; 
+}
+
+public int getQuantidadePaginas(){
+    return quantidadePaginas; 
+}
+
+public void setQuantidadePaginas(int quantidadePaginas) {
+    this.quantidadePaginas = quantidadePaginas; 
+
+}
+
+// calcular tempo de leitura : 
+public int calcularTempoLeitura(){
+    return quantidadePaginas; 
+}
+
+// Metodos para exibir informações :
+public void exibirInformacoes() {
+  System.out.println("Titulos :" + titulo);
+  System.out.println("Autor : " + autor);
+  System.out.println("Ano de Publicação :" + anoPublicacao);
+  System.out.println("Paginas : " + quantidadePaginas);
+  System.out.println("Tempo estimado de leitura : " + calcularTempoLeitura() + " minutos");
+}
+
+
+
+
+
+
+
+}
